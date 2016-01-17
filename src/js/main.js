61a4346ba60911b287970ff59970808b53f0b5a7
RTeran <rafaelterantorralbo@gmail.com>
2016-01-17 21:04:23 +0100

    add header and footer components

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
+function activeClass(url) {
+	var urlWithoutSlash = url.slice(1);
+	$("nav").find("a[href='" + urlWithoutSlash + "']").parent().addClass('active');
+}
+
+$(function(){
+	$( "nav" ).load( "components/header.html", function(){
+		activeClass(window.location.pathname);
+	} );
+
+	$( "footer" ).load( "components/footer.html");
+});
+
 $(".button-collapse").sideNav();

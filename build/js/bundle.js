533f34b870fc4c09b46ca97d55f075318d1dbd1f
Angie Ecija <Angie Ecija>
2016-01-20 21:10:42 +0100

    styles of cards on instrumentation

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -4,17 +4,40 @@ function activeClass(url) {
 	$("nav").find("a[href='" + urlWithoutSlash + "']").parent().addClass('active');
 }
 
+function openMod(id) {
+	$(id).openModal();
+}
+
+
 $(function(){
 	$( "nav" ).load( "components/header.html", function(){
 		activeClass(window.location.pathname);
 	} );
 
 	$( "footer" ).load( "components/footer.html");
+
+	if (window.location.pathname.indexOf('instrumentation') >= 0){
+		/*
+		 * jQuery Succinct plugin
+		 * Version 1.1.0 (October 2014)
+		 */
+		!function(a){"use strict";a.fn.succinct=function(b){var c=a.extend({size:240,omission:"...",ignore:!0},b);return this.each(function(){var b,d,e=a(this),f=/[!-\/:-@\[-`{-~]$/,g=function(){e.each(function(){b=a(this).html(),b.length>c.size&&(d=a.trim(b).substring(0,c.size).split(" ").slice(0,-1).join(" "),c.ignore&&(d=d.replace(f,"")),a(this).html(d+c.omission))})};g()})}}(jQuery);
+	
+		$('.ellipsis-short').succinct({
+			size: 190
+		});
+	}
+
+	$('.card-action a').click(function() {
+		var id = $(this).attr('href');
+		openMod(id);
+	});
 });
 
 $(".button-collapse").sideNav();
 
-$(document).ready(function(){
-      $('.carousel').carousel();
-    });
+
+
+
+
 },{}]},{},["C:\\Users\\Angie\\Documents\\webs\\fqm303\\src\\js\\main"]);

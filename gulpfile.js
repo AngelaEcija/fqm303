274db975b2b6f7868db642ed5f05b1bc73b94b48
AngelaEcija <angie.ecija.4@gmail.com>
2019-06-08 19:58:32 +0200

    upgrade: upgraded npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,84 @@
-var browserSync  = require('browser-sync');
-var watchify     = require('watchify');
-var browserify   = require('browserify');
-var source       = require('vinyl-source-stream');
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var gulpSequence = require('gulp-sequence');
-var processhtml  = require('gulp-minify-html');
-var sass         = require('gulp-sass');
-var watch        = require('gulp-watch');
-var minifycss    = require('gulp-minify-css');
-var uglify       = require('gulp-uglify');
-var streamify    = require('gulp-streamify');
-var prod         = gutil.env.prod;
+var browserSync = require("browser-sync");
+var watchify = require("watchify");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var gulpSequence = require("gulp-sequence");
+var processhtml = require("gulp-minify-html");
+var sass = require("gulp-sass");
+var minifycss = require("gulp-minify-css");
+var uglify = require("gulp-uglify");
+var streamify = require("gulp-streamify");
+var prod = gutil.env.prod;
 
 var onError = function(err) {
   console.log(err.message);
-  this.emit('end');
+  this.emit("end");
 };
 
 // bundling js with browserify and watchify
-var b = watchify(browserify('./src/js/main', {
-  cache: {},
-  packageCache: {},
-  fullPaths: true
-}));
+var b = watchify(
+  browserify("./src/js/main", {
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  })
+);
 
-gulp.task('js', bundle);
-b.on('update', bundle);
-b.on('log', gutil.log);
+gulp.task("js", bundle);
+b.on("update", bundle);
+b.on("log", gutil.log);
 
 function bundle() {
-  return b.bundle()
-    .on('error', onError)
-    .pipe(source('bundle.js'))
+  return b
+    .bundle()
+    .on("error", onError)
+    .pipe(source("bundle.js"))
     .pipe(prod ? streamify(uglify()) : gutil.noop())
-    .pipe(gulp.dest('./build/js'))
+    .pipe(gulp.dest("./build/js"))
     .pipe(browserSync.stream());
 }
 
 // html
-gulp.task('html', function() {
-  return gulp.src('./src/templates/**/*')
+gulp.task("html", function() {
+  return gulp
+    .src("./src/templates/**/*")
     .pipe(processhtml())
-    .pipe(gulp.dest('build'))
+    .pipe(gulp.dest("build"))
     .pipe(browserSync.stream());
 });
 
 // htaccess
-gulp.task('htaccess', function() {
-  return gulp.src('./src/.htaccess')
-    .pipe(gulp.dest('build'))
+gulp.task("htaccess", function() {
+  return gulp
+    .src("./src/.htaccess")
+    .pipe(gulp.dest("build"))
     .pipe(browserSync.stream());
 });
 
 // sass
-gulp.task('sass', function() {
-  gulp.src('./src/scss/**/*.scss')
+gulp.task("sass", function() {
+  gulp
+    .src("./src/scss/**/*.scss")
     .pipe(sass())
-    .on('error', sass.logError)
+    .on("error", sass.logError)
     .pipe(prod ? minifycss() : gutil.noop())
-    .pipe(gulp.dest('./build/stylesheets'))
+    .pipe(gulp.dest("./build/stylesheets"))
     .pipe(browserSync.stream());
 });
 
 // browser sync server for live reload
-gulp.task('serve', function() {
+gulp.task("serve", function() {
   browserSync.init({
     server: {
-      baseDir: './build'
+      baseDir: "./build"
     }
   });
 
-  gulp.watch('./src/templates/**/*', ['html']);
-  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch("./src/templates/**/*", ["html"]);
+  gulp.watch("./src/scss/**/*.scss", ["sass"]);
 });
 
 // use gulp-sequence to finish building html, sass and js before first page load
-gulp.task('default', gulpSequence(['htaccess', 'html', 'sass', 'js'], 'serve'));
+gulp.task("default", gulpSequence(["htaccess", "html", "sass", "js"], "serve"));

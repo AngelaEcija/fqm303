6fb8b322cde3f7f18ff7f1b00bbcc843d2cc6aaa
Angela Ecija Arenas <angie.ecija.4@gmail.com>
2020-10-20 12:42:37 +0200

    research-group section remodeling

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,4 +1,4 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"/Users/AEcija/Documents/webs/fqm303/src/js/main":[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({"/Users/aecija/Documents/webs/fqm303/src/js/main":[function(require,module,exports){
 function activeClass(url) {
   var urlWithoutSlash = url.split("grupos/FQM-303/");
   $("nav")
@@ -72,4 +72,4 @@ $(function() {
   });
 });
 
-},{}]},{},["/Users/AEcija/Documents/webs/fqm303/src/js/main"]);
+},{}]},{},["/Users/aecija/Documents/webs/fqm303/src/js/main"]);
